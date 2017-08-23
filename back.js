203bdfeff1b7f234459992ccbaa9de8f78bc35b4
patelmihir021021 <patelmihir021021@gmail.com>
2017-08-22 23:52:00 -0400

    Add files via upload

---
+++ back.js
@@ -0,0 +1,38 @@
+function contact() {
+    var x = document.getElementById('call');
+	var y = document.getElementById('email');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+		y.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+		y.style.display = 'none';
+    }
+}
+
+function project() {
+    var x = document.getElementById('sm');
+	var y = document.getElementById('cc');
+	var z = document.getElementById('wtc');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+		y.style.display = 'block';
+		z.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+		y.style.display = 'none';
+		z.style.display = 'none';
+    }
+}
+function contact() {
+    var x = document.getElementById('call');
+	var y = document.getElementById('email');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+		y.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+		y.style.display = 'none';
+    }
+}
+
